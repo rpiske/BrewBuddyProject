0e56b173ceb08aff0fc63cbad671e5f1719ff96f
rpiske <rpiske@my.ccsu.edu>
2023-04-14 14:45:02 -0400

    Added RecyclerView Functionality
    I'm having problems getting the actual recycler view to display on the second activity.  No idea why nothing is showing up.  Emailing Prof for assistance.

--- BrewBuddy/app/src/main/java/com/example/brewbuddy/MainActivity.kt
+++ BrewBuddy/app/src/main/java/com/example/brewbuddy/MainActivity.kt
@@ -1,9 +1,12 @@
 package com.example.brewbuddy
 
 import android.content.ContentValues.TAG
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
+import android.widget.EditText
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -12,40 +15,20 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 class MainActivity : AppCompatActivity() {
 
-    //TEST COMMENT
-    //This comment begins RecyclerView Branch
 
-    val BASE_URL = "https://api.openbrewerydb.org/v1/"
     private val TAG = "MainActivity"
-    val breweryLocations = ArrayList<Brewery>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val retrofitBuilder = Retrofit.Builder()
-            .baseUrl(BASE_URL)
-            .addConverterFactory(GsonConverterFactory.create())
-            .build()
+        findViewById<Button>(R.id.search_by_zip_button).setOnClickListener {
 
-        val breweryLocationsAPI = retrofitBuilder.create(BreweryService::class.java)
+            val zipCode = findViewById<EditText>(R.id.enter_zip).text.toString()
 
-        breweryLocationsAPI.getBreweryByZip("44107", 3).enqueue(object : Callback<List<Brewery>?> {
-            override fun onResponse(call: Call<List<Brewery>?>, response: Response<List<Brewery>?>) {
-                Log.d(TAG, "onResponse: $response")
-                val body = response.body()
-                if(body == null) {
-                    Log.d(TAG, "Valid response was not received")
-                    return
-                }
-                breweryLocations.addAll(body)
-                Log.d(TAG, "a: ${breweryLocations[0].name}")
-                Log.d(TAG, "b: ${breweryLocations[1].name}")
-                Log.d(TAG, "c: ${breweryLocations[2].name}")
-            }
-
-            override fun onFailure(call: Call<List<Brewery>?>, t: Throwable) {
-                Log.d(TAG, "onResponse: $t")
-            }
-        })
+            val myIntent = Intent(this, ResultsActivity::class.java)
+            myIntent.putExtra("zipCode", zipCode)
+            startActivity(myIntent)
+        }
     }
 }
